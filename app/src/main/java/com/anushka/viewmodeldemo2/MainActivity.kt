ef2e326de6e2e727b6b3e4710aacea677760bc92
ADI <adinh@jiway.lu>
2021-09-11 19:03:17 +0200

    Implementing ViewModel

--- app/src/main/java/com/anushka/viewmodeldemo2/MainActivity.kt
+++ app/src/main/java/com/anushka/viewmodeldemo2/MainActivity.kt
@@ -3,17 +3,25 @@ package com.anushka.viewmodeldemo2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.anushka.viewmodeldemo2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
-        binding.btnAdd.setOnClickListener{
+        binding.tvResult.text = viewModel.getCurrentSum().toString()
 
+        binding.btnAdd.setOnClickListener {
+            if (binding.etInput.text.toString().isNotBlank())
+                binding.tvResult.text =
+                    viewModel.getModifiedSum(Integer.valueOf(binding.etInput.text.toString()))
+                        .toString()
         }
     }
 }
